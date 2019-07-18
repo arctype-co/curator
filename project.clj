06d5816a2a7198fdf8b4009f31b9acba30dec641
Ryan Sundberg <ryan.sundberg@gmail.com>
2019-07-17 19:44:18 -0700

    Upgrade dependencies, including apache/curator to 4.2.1

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject arctype/curator "0.1.0-SNAPSHOT"
   :description "Clojurified Apache Curator"
-  :url "https://github.com/pingles/curator"
+  :url "https://github.com/arctype-co/curator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.curator/curator-recipes "3.2.1"]
-                 [org.apache.curator/curator-framework "3.2.1"]
-                 [org.apache.curator/curator-x-discovery "3.2.1"]]
-  :profiles {:dev {:dependencies [[org.slf4j/log4j-over-slf4j "1.7.24"]
-                                  [org.slf4j/slf4j-simple "1.7.24"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.apache.curator/curator-recipes "4.2.0"]
+                 [org.apache.curator/curator-framework "4.2.0"]
+                 [org.apache.curator/curator-x-discovery "4.2.0"]]
+  :profiles {:dev {:dependencies [[org.slf4j/log4j-over-slf4j "1.7.26"]
+                                  [org.slf4j/slf4j-simple "1.7.26"]]
                    :exclusions [org.slf4j/slf4j-log4j12]}}
   :scm {:name "git"
-        :url "https://github.com/pingles/curator"})
+        :url "https://github.com/arctype-co/curator"})
